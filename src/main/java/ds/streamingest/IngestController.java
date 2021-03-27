d2462603dcc012c8842d957fb050a1208de047fb
d-smith <doug.smith.mail@gmail.com>
2021-03-27 16:55:33 +0000

    added logging support

--- src/main/java/ds/streamingest/IngestController.java
+++ src/main/java/ds/streamingest/IngestController.java
@@ -6,12 +6,19 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+
 @RestController
 public class IngestController {
+    private final static Logger logger = LoggerFactory.getLogger(IngestController.class);
+    
+    
     @PostMapping("/writeToStream")
     public String ingest(@RequestBody WriteStreamRequest writeStreamRequest) {
-        System.out.println(writeStreamRequest);
-        System.out.println("key is " + writeStreamRequest.getKey());
+        logger.info("key is " + writeStreamRequest.getKey());
         return "got it";
     }
 }
