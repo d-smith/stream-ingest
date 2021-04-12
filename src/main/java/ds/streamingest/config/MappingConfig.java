19242581356f622f97db75736ae7c1bdb76a1027
d-smith <doug.smith.mail@gmail.com>
2021-04-12 07:01:15 -0700

    added prop config load for mappings

--- src/main/java/ds/streamingest/config/MappingConfig.java
+++ src/main/java/ds/streamingest/config/MappingConfig.java
@@ -1,25 +1,29 @@
 package ds.streamingest.config;
 
+import ds.streamingest.model.ApiMapping;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @ConfigurationProperties("mapping")
 public class MappingConfig {
-    private String test;
+    private List<ApiMapping> streams = new ArrayList<>();
 
-    public String getTest() {
-        return test;
+    public List<ApiMapping> getStreams() {
+        return streams;
     }
 
-    public void setTest(String test) {
-        this.test = test;
+    public void setStreams(List<ApiMapping> streams) {
+        this.streams = streams;
     }
 
     @Override
     public String toString() {
         return "MappingConfig{" +
-                "test='" + test + '\'' +
+                "streams=" + streams +
                 '}';
     }
 }
